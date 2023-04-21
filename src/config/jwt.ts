96b4a9c906000e75bd663d39ccea1464e299e4a7
Jakob Lux <luja1012@h-ka.de>
2023-04-21 18:17:47 +0200

    config2

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 import { type SignOptions, type VerifyOptions } from 'jsonwebtoken';
 import { configDir } from './node.js';
 import { env } from './env.js';
@@ -64,7 +47,7 @@ const { JWT_EXPIRES_IN, JWT_ISSUER } = env;
 const signOptions: SignOptions = {
     algorithm,
     expiresIn: JWT_EXPIRES_IN ?? '1h',
-    issuer: JWT_ISSUER ?? 'https://hka.de/JuergenZimmermann',
+    issuer: JWT_ISSUER ?? 'https://hka.de/Gruppe7',
 };
 
 const verifyOptions: VerifyOptions = {
