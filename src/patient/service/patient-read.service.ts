463aa11c4b03a5613ae96853926ff13aef28b4d9
Jakob Lux <luja1012@h-ka.de>
2023-04-21 12:51:22 +0200

    ReadService-Start

---
+++ src/patient/service/patient-read.service.ts
@@ -0,0 +1,33 @@
+import { PatientVersicherungsart } from "../entity/patient.entity";
+
+/**
+ * Typdefinition für `findById`
+ */
+export interface FindByIdParams {
+    /** ID des gesuchten Buchs */
+    id: number;
+    /** Sollen die Operationen mitgeladen werden? */
+    mitOperationen?: boolean;
+}
+/**
+ * Suchkriterien für `findById`
+ */
+// TODO gehört 'name' hier rein?
+export interface Suchkriterien {
+    readonly versichertennummer?: number;
+    readonly versicherungsart?: PatientVersicherungsart;
+    readonly geburtsdatum?: Date;
+    readonly intensiv?: boolean;
+    readonly diagnose?: string;
+    readonly name?: string;
+}
+
+/**
+ * Die Klasse `PatientReadService` implementiert das Lesen für Patienten und greift
+ * mit _TypeORM_ auf eine relationale DB zu.
+ */
+export class PatientReadService {
+    async findById({ id, mitOperationen = false }: FindByIdParams) {
+        // Warten auf Logger
+    }
+}
