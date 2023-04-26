133b83d305ee4bf3cef0d6fed23440cb2766cbc3
Jakob Lux <luja1012@h-ka.de>
2023-04-26 17:39:05 +0200

    GET-Many

---
+++ __tests__/patient/patient-GET.controller.test.ts
@@ -0,0 +1,127 @@
+/* eslint-disable no-underscore-dangle */
+
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { HttpStatus } from '@nestjs/common';
+import { type PatientenModel } from '../../src/patient/rest/patient-get.controller.js';
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const nameVorhanden = 'a';
+const nameNichtVorhanden = 'xx';
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('GET /rest', () => {
+    let baseURL: string;
+    let client: AxiosInstance;
+
+    beforeAll(async () => {
+        await startServer();
+        baseURL = `https://${host}:${port}/rest`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+            validateStatus: () => true,
+        });
+    });
+
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    test('Alle Patienten', async () => {
+        // given
+
+        // when
+        const response: AxiosResponse<PatientenModel> = await client.get('/');
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data).toBeDefined();
+
+        const { patienten } = data._embedded;
+
+        patienten
+            .map((patient) => patient._links.self.href)
+            .forEach((selfLink) => {
+                // eslint-disable-next-line security/detect-non-literal-regexp, security-node/non-literal-reg-expr
+                expect(selfLink).toMatch(new RegExp(`^${baseURL}`, 'u'));
+            });
+    });
+
+    test('Patienten mit einem Teil-Namen suchen', async () => {
+        // given
+        const params = { name: nameVorhanden };
+
+        // when
+        const response: AxiosResponse<PatientenModel> = await client.get('/', {
+            params,
+        });
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data).toBeDefined();
+
+        const { patienten } = data._embedded;
+
+        // Jeder Patient hat einen Namen mit dem Teilstring 'a'
+        patienten
+            .map((patient) => patient.name)
+            .forEach((name) =>
+                expect(name.nachname.toLowerCase()).toEqual(
+                    expect.stringContaining(nameVorhanden),
+                ),
+            );
+    });
+
+    test('Patienten zu einem nicht vorhandenen Teil-Namen suchen', async () => {
+        // given
+        const params = { name: nameNichtVorhanden };
+
+        // when
+        const response: AxiosResponse<string> = await client.get('/', {
+            params,
+        });
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.NOT_FOUND);
+        expect(data).toMatch(/^not found$/iu);
+    });
+
+    test('Keine Patienten zu einer nicht-vorhandenen Property', async () => {
+        // given
+        const params = { foo: 'bar' };
+
+        // when
+        const response: AxiosResponse<string> = await client.get('/', {
+            params,
+        });
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.NOT_FOUND);
+        expect(data).toMatch(/^not found$/iu);
+    });
+});
+/* eslint-enable no-underscore-dangle */
