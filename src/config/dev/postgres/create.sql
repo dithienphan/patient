7d68152f18cf24dbaa0613c8c3635c87cc1e886d
Dinh Thien Phan <dithienphan@gmail.com>
2023-04-26 20:46:23 +0200

    Test(ohne MySQL) und Datenbank Scripte

---
+++ src/config/dev/postgres/create.sql
@@ -0,0 +1,32 @@
+CREATE SCHEMA IF NOT EXISTS AUTHORIZATION patient;
+
+ALTER ROLE patient SET search_path = 'patient';
+
+CREATE TYPE versicherungsart AS ENUM ('GESETZLICH', 'PRIVAT');
+
+CREATE TABLE IF NOT EXISTS patient (
+    id                      integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE patientspace,
+    version                 integer NOT NULL DEFAULT 0,
+    versichertennummer      varchar(11) NOT NULL UNIQUE USING INDEX TABLESPACE patientspace,
+    versicherungsart        versicherungsart,
+    intensiv                boolean NOT NULL DEFAULT FALSE,
+    geburtsdatum            date,
+    diagnose                varchar(40),
+    erzeugt                 timestamp NOT NULL DEFAULT NOW(),
+    aktualisiert            timestamp NOT NULL DEFAULT NOW()
+) TABLESPACE patientspace;
+
+CREATE TABLE IF NOT EXISTS name (
+    id              integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE patientspace,
+    nachname        varchar(32) NOT NULL,
+    vorname         varchar(32) NOT NULL,
+    patient_id      integer NOT NULL UNIQUE USING INDEX TABLESPACE patientspace REFERENCES patient
+) TABLESPACE patientspace;
+
+CREATE TABLE IF NOT EXISTS operation (
+    id                  integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE patientspace,
+    eingriff            varchar(64) NOT NULL,
+    behandlungsraum     integer NOT NULL,
+    patient_id          integer NOT NULL REFERENCES patient
+) TABLESPACE patientspace;
+CREATE INDEX IF NOT EXISTS operation_patient_id_idx ON operation(patient_id) TABLESPACE patientspace;
