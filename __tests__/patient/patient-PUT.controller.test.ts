b6a9c00d6068053b428763174c2902704f8f8746
Jakob Lux <luja1012@h-ka.de>
2023-04-30 15:19:29 +0200

    PUT Test & mutation Test

---
+++ __tests__/patient/patient-PUT.controller.test.ts
@@ -0,0 +1,237 @@
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { HttpStatus } from '@nestjs/common';
+import { type PatientDtoOhneRef } from '../../src/patient/rest/patientDTO.entity.js';
+import { loginRest } from '../login.js';
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const geaenderterPatient: PatientDtoOhneRef = {
+    versichertennummer: 'A123456789',
+    versicherungsart: 'PRIVAT',
+    intensiv: false,
+    geburtsdatum: '2005-01-01',
+    diagnose: 'deja-vu',
+};
+const idVorhanden = '30';
+
+const geaenderterPatientIdNichtVorhanden: PatientDtoOhneRef = {
+    versichertennummer: 'H135792468',
+    versicherungsart: 'GESETZLICH',
+    intensiv: false,
+    geburtsdatum: '2000-01-01',
+    diagnose: 'nicht-vorhanden',
+};
+const idNichtVorhanden = '999999';
+
+const geaenderterPatientInvalid: Record<string, unknown> = {
+    versichertennummer: 'V-falsch-invalid',
+    versicherungsart: 'GESETZLICH',
+    intensiv: true,
+    geburtsdatum: '2012-01-01',
+    diagnose: 'nicht-vorhanden',
+};
+
+const veralterPatient: PatientDtoOhneRef = {
+    versichertennummer: 'A123456789',
+    versicherungsart: 'PRIVAT',
+    intensiv: false,
+    geburtsdatum: '2005-01-01',
+    diagnose: 'deja-vu',
+};
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('PUT /rest/:id', () => {
+    let client: AxiosInstance;
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json', // eslint-disable-line @typescript-eslint/naming-convention
+    };
+
+    // Testserver starten und dabei mit der DB verbinden
+    beforeAll(async () => {
+        await startServer();
+        const baseURL = `https://${host}:${port}`;
+        client = axios.create({
+            baseURL,
+            headers,
+            httpsAgent,
+            validateStatus: (status) => status < 500, // eslint-disable-line @typescript-eslint/no-magic-numbers
+        });
+    });
+
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    test('Vorhandenen Patient aendern', async () => {
+        // given
+        const url = `/rest/${idVorhanden}`;
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+        headers['If-Match'] = '"0"';
+
+        // when
+        const response: AxiosResponse<string> = await client.put(
+            url,
+            geaenderterPatient,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.NO_CONTENT);
+        expect(data).toBe('');
+    });
+
+    test('Nicht-vorhandenes Patient aendern', async () => {
+        // given
+        const url = `/rest/${idNichtVorhanden}`;
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+        headers['If-Match'] = '"0"';
+
+        // when
+        const response: AxiosResponse<string> = await client.put(
+            url,
+            geaenderterPatientIdNichtVorhanden,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.PRECONDITION_FAILED);
+        expect(data).toBe(
+            `Es gibt kein Patient mit der ID "${idNichtVorhanden}".`,
+        );
+    });
+
+    test('Vorhandenen Patient aendern, aber mit ungueltigen Daten', async () => {
+        // given
+        const url = `/rest/${idVorhanden}`;
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+        headers['If-Match'] = '"0"';
+        const expectedMsg = [
+            expect.stringMatching(/^versichertennummer /u),
+            expect.stringMatching(/^versicherungsart /u),
+            expect.stringMatching(/^intensiv /u),
+            expect.stringMatching(/^geburtsdatum /u),
+            expect.stringMatching(/^diagnose /u),
+        ];
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.put(
+            url,
+            geaenderterPatientInvalid,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const messages: string[] = data.message;
+
+        expect(messages).toBeDefined();
+        expect(messages).toHaveLength(expectedMsg.length);
+        expect(messages).toEqual(expect.arrayContaining(expectedMsg));
+    });
+
+    test('Vorhandenen Patient aendern, aber ohne Versionsnummer', async () => {
+        // given
+        const url = `/rest/${idVorhanden}`;
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+        delete headers['If-Match'];
+
+        // when
+        const response: AxiosResponse<string> = await client.put(
+            url,
+            geaenderterPatient,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.PRECONDITION_REQUIRED);
+        expect(data).toBe('Header "If-Match" fehlt');
+    });
+
+    test('Vorhandenen Patient aendern, aber mit alter Versionsnummer', async () => {
+        // given
+        const url = `/rest/${idVorhanden}`;
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+        headers['If-Match'] = '"-1"';
+
+        // when
+        const response: AxiosResponse<string> = await client.put(
+            url,
+            veralterPatient,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.PRECONDITION_FAILED);
+        expect(data).toEqual(expect.stringContaining('Die Versionsnummer'));
+    });
+
+    test('Vorhandenen Patient aendern, aber ohne Token', async () => {
+        // given
+        const url = `/rest/${idVorhanden}`;
+        delete headers.Authorization;
+        headers['If-Match'] = '"0"';
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.put(
+            url,
+            geaenderterPatient,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.FORBIDDEN);
+        expect(data.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+
+    test('Vorhandenen Patient aendern, aber mit falschem Token', async () => {
+        // given
+        const url = `/rest/${idVorhanden}`;
+        const token = 'FALSCH';
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.put(
+            url,
+            geaenderterPatient,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.FORBIDDEN);
+        expect(data.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+});
---
+++ __tests__/patient/patient-mutation.resolver.test.ts
@@ -0,0 +1,330 @@
+/* eslint-disable max-lines, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-extra-non-null-assertion */
+
+import {
+    type GraphQLQuery,
+    type GraphQLResponseBody,
+} from './patient-query.resolver.test.js';
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { HttpStatus } from '@nestjs/common';
+import { PatientReadService } from '../../src/patient/service/patient-read.service.js';
+import { loginGraphQL } from '../login.js';
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const idLoeschen = '60';
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('GraphQL Mutations', () => {
+    let client: AxiosInstance;
+    const graphqlPath = 'graphql';
+
+    // Testserver starten und dabei mit der DB verbinden
+    beforeAll(async () => {
+        await startServer();
+        const baseURL = `https://${host}:${port}/`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+        });
+    });
+
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    // -------------------------------------------------------------------------
+    test('Neuer Patient', async () => {
+        // given
+        const token = await loginGraphQL(client);
+        const authorization = { Authorization: `Bearer ${token}` }; // eslint-disable-line @typescript-eslint/naming-convention
+        const body: GraphQLQuery = {
+            query: `
+                mutation {
+                    create(
+                        input: {
+                            // TODO input
+                        }
+                    )
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+            { headers: authorization },
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.data).toBeDefined();
+
+        const { create } = data.data!;
+
+        // Der Wert der Mutation ist die generierte ObjectID
+        expect(create).toBeDefined();
+        expect(PatientReadService.ID_PATTERN.test(create as string)).toBe(true);
+    });
+
+    // -------------------------------------------------------------------------
+    // eslint-disable-next-line max-lines-per-function
+    test('Patient mit ungueltigen Werten neu anlegen', async () => {
+        // given
+        const token = await loginGraphQL(client);
+        const authorization = { Authorization: `Bearer ${token}` }; // eslint-disable-line @typescript-eslint/naming-convention
+        const body: GraphQLQuery = {
+            query: `
+                mutation {
+                    create(
+                        input: {
+                            // TODO input
+                        }
+                    )
+                }
+            `,
+        };
+        const expectedMsg = [
+            expect.stringMatching(/^versichertennummer /u),
+            expect.stringMatching(/^versicherungsart /u),
+            expect.stringMatching(/^intensiv /u),
+            expect.stringMatching(/^geburtsdatum /u),
+            expect.stringMatching(/^diagnose /u),
+            expect.stringMatching(/^name.nachname /u),
+        ];
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+            { headers: authorization },
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.data!.create).toBeNull();
+
+        const { errors } = data;
+
+        expect(errors).toHaveLength(1);
+
+        const [error] = errors!;
+        const extensions: any = error?.extensions;
+
+        expect(extensions).toBeDefined();
+
+        const messages: string[] = extensions?.originalError?.message;
+
+        expect(messages).toBeDefined();
+        expect(messages).toHaveLength(expectedMsg.length);
+        expect(messages).toEqual(expect.arrayContaining(expectedMsg));
+    });
+
+    // -------------------------------------------------------------------------
+    test('Neuer Patient nur als "arzt"/"rezeptionist"', async () => {
+        // given
+        const token = await loginGraphQL(client, 'dirk.delta', 'p');
+        const authorization = { Authorization: `Bearer ${token}` }; // eslint-disable-line @typescript-eslint/naming-convention
+        const body: GraphQLQuery = {
+            query: `
+                mutation {
+                    create(
+                        input: {
+                            // TODO input
+                        }
+                    )
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+            { headers: authorization },
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+
+        const { errors } = data;
+
+        expect(errors).toHaveLength(1);
+
+        const [error] = errors!;
+        const { message, extensions } = error!;
+
+        expect(message).toBe('Forbidden resource');
+        expect(extensions).toBeDefined();
+        expect(extensions!.code).toBe('FORBIDDEN');
+    });
+
+    // -------------------------------------------------------------------------
+    test('Patienten aktualisieren', async () => {
+        // given
+        const token = await loginGraphQL(client);
+        const authorization = { Authorization: `Bearer ${token}` }; // eslint-disable-line @typescript-eslint/naming-convention
+        const body: GraphQLQuery = {
+            query: `
+                mutation {
+                    update(
+                        input: {
+                            // TODO input
+                        }
+                    )
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+            { headers: authorization },
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.errors).toBeUndefined();
+
+        const { update } = data.data!;
+
+        // Der Wert der Mutation ist die neue Versionsnummer
+        expect(update).toBe(1);
+    });
+
+    // -------------------------------------------------------------------------
+    // eslint-disable-next-line max-lines-per-function
+    test('Patienten mit ungueltigen Werten aktualisieren', async () => {
+        // given
+        const token = await loginGraphQL(client);
+        const authorization = { Authorization: `Bearer ${token}` }; // eslint-disable-line @typescript-eslint/naming-convention
+        const id = '40';
+        const body: GraphQLQuery = {
+            query: `
+                mutation {
+                    update(
+                        input: {
+                            id: "${id}",
+                            version: 0,
+                            // TODO input
+                        }
+                    )
+                }
+            `,
+        };
+        const expectedMsg = [
+            expect.stringMatching(/^isbn /u),
+            expect.stringMatching(/^rating /u),
+            expect.stringMatching(/^preis /u),
+            expect.stringMatching(/^rabatt /u),
+            expect.stringMatching(/^datum /u),
+            expect.stringMatching(/^homepage /u),
+        ];
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+            { headers: authorization },
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.data!.update).toBeNull();
+
+        const { errors } = data;
+
+        expect(errors).toHaveLength(1);
+
+        const [error] = errors!;
+        const extensions: any = error?.extensions;
+
+        expect(extensions).toBeDefined();
+
+        const messages: string[] = extensions.originalError.message;
+
+        expect(messages).toBeDefined();
+        expect(messages).toHaveLength(expectedMsg.length);
+        expect(messages).toEqual(expect.arrayContaining(expectedMsg));
+    });
+
+    // -------------------------------------------------------------------------
+    test('Nicht-vorhandenen Patienten aktualisieren', async () => {
+        // given
+        const token = await loginGraphQL(client);
+        const authorization = { Authorization: `Bearer ${token}` }; // eslint-disable-line @typescript-eslint/naming-convention
+        const id = '999999';
+        const body: GraphQLQuery = {
+            query: `
+                mutation {
+                    update(
+                        input: {
+                            id: "${id}",
+                            version: 0,
+                            // TODO input
+                        }
+                    )
+                }
+            `,
+        };
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+            { headers: authorization },
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data.data!.update).toBeNull();
+
+        const { errors } = data;
+
+        expect(errors).toHaveLength(1);
+
+        const [error] = errors!;
+        const { message, path, extensions } = error!;
+
+        expect(message).toBe(
+            `Es gibt keinen Patienten mit der ID ${id.toLowerCase()}`,
+        );
+        expect(path).toBeDefined();
+        expect(path!![0]).toBe('update');
+        expect(extensions).toBeDefined();
+        expect(extensions!.code).toBe('BAD_USER_INPUT');
+    });
+});
+/* eslint-enable max-lines, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-extra-non-null-assertion */
