fffeafc8f047978856aaefead75fd7d0da51aff0
Achim Seelhorst <seac1011@h-ka.de>
2023-05-03 09:36:58 +0200

    Post Patient

---
+++ __tests__/patient/patient-POST.controller.test.ts
@@ -0,0 +1,228 @@
+/*
+ * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { type PatientDTO } from '../../src/patient/rest/patientDTO.entity.js';
+import { PatientReadService } from '../../src/patient/service/patient-read.service.js';
+import { HttpStatus } from '@nestjs/common';
+import { loginRest } from '../login.js';
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const neuerPatient: PatientDTO = {
+    versichertennummer: 'Z987654322',
+    versicherungsart: 'GESETZLICH',
+    intensiv: true,
+    geburtsdatum: '1970-10-10',
+    diagnose: 'Atmungsprobleme',
+    name: {
+        vorname: 'Peter',
+        nachname: 'Parker',
+    },
+    operationen: [
+        {
+            eingriff: 'Herz',
+            behandlungsraum: 100,
+        },
+    ],
+};
+const neuerPatientInvalid: Record<string, unknown> = {
+    versichertennummer: 'falsche-versichertennummer',
+    versicherungsart: 'UNSICHTBAR',
+    intensiv: true,
+    datum: '1111-90-90',
+    name: {
+        vorname: 'Steven',
+        nachname: 'Rogers',
+    },
+};
+const neuerPatientVersichertennummerExistiert: PatientDTO = {
+    versichertennummer: 'Z987654321',
+    versicherungsart: 'GESETZLICH',
+    intensiv: true,
+    geburtsdatum: '2022-02-28',
+    diagnose: 'Atmungsschwierigkeiten',
+    name: {
+        vorname: 'Barry',
+        nachname: 'Allen',
+    },
+    operationen: [
+        {
+            eingriff: 'Lunge',
+            behandlungsraum: 102,
+        },
+    ],
+};
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('POST /rest', () => {
+    let client: AxiosInstance;
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json', // eslint-disable-line @typescript-eslint/naming-convention
+    };
+
+    // Testserver starten und dabei mit der DB verbinden
+    beforeAll(async () => {
+        await startServer();
+        const baseURL = `https://${host}:${port}`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+            validateStatus: (status) => status < 500, // eslint-disable-line @typescript-eslint/no-magic-numbers
+        });
+    });
+
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    test('Neuer Patient', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<string> = await client.post(
+            '/rest',
+            neuerPatient,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.CREATED);
+
+        const { location } = response.headers as { location: string };
+
+        expect(location).toBeDefined();
+
+        // ObjectID: Muster von HEX-Ziffern
+        const indexLastSlash: number = location.lastIndexOf('/');
+
+        expect(indexLastSlash).not.toBe(-1);
+
+        const idStr = location.slice(indexLastSlash + 1);
+
+        expect(idStr).toBeDefined();
+        expect(PatientReadService.ID_PATTERN.test(idStr)).toBe(true);
+
+        expect(data).toBe('');
+    });
+
+    test('Neuer Patient mit ungueltigen Daten', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+        const expectedMsg = [
+            expect.stringMatching(/^versichertennummer /u),
+            expect.stringMatching(/^rating /u),
+            expect.stringMatching(/^versicherungsart /u),
+            expect.stringMatching(/^preis /u),
+            expect.stringMatching(/^rabatt /u),
+            expect.stringMatching(/^datum /u),
+            expect.stringMatching(/^homepage /u),
+            expect.stringMatching(/^titel.titel /u),
+        ];
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuerPatientInvalid,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const messages: string[] = data.message;
+
+        expect(messages).toBeDefined();
+        expect(messages).toHaveLength(expectedMsg.length);
+        expect(messages).toEqual(expect.arrayContaining(expectedMsg));
+    });
+
+    test('Neuer Patient, aber die versichertennummer existiert bereits', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<string> = await client.post(
+            '/rest',
+            neuerPatientVersichertennummerExistiert,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+        expect(data).toEqual(expect.stringContaining('Versichertennummer'));
+    });
+
+    test('Neuer Patient, aber ohne Token', async () => {
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuerPatient,
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.FORBIDDEN);
+        expect(data.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+
+    test('Neuer Patient, aber mit falschem Token', async () => {
+        // given
+        const token = 'FALSCH';
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuerPatient,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.FORBIDDEN);
+        expect(data.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+
+    test.todo('Abgelaufener Token');
+});
